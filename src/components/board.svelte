1c040b9166be5a587c81aa91e11741c0cc72ff1b
Adrian Stobbe <stobbe.adrian@gmail.com>
2021-12-17 15:30:48 +0100

    board component

---
+++ src/components/board.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Cell from '../components/cell.svelte';
+	export let position;
+	export let orientation;
+	let columns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+	function getIdx(i, j) {
+		return i * columns.length + j;
+	}
+
+	function isRoverPosition(i, j) {
+		return getIdx(i, j) === getIdx(position.x, position.y);
+	}
+</script>
+
+<table>
+	{#each columns as i}
+		<tr id="r{i}">
+			{#each columns as j}
+				<Cell id={getIdx(i, j)}>{isRoverPosition(i, j) ? orientation : ''}</Cell>
+			{/each}
+		</tr>
+	{/each}
+</table>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,9 @@
 <script>
 	import Cell from '../components/cell.svelte';
+	import Board from '../components/board.svelte';
 	let position = { x: 1, y: 0 };
-	let columns = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-	function getIdx(i, j) {
-		return i * columns.length + j;
-	}
-
-	function isRoverPosition(i, j) {
-		return getIdx(i, j) === getIdx(position.x, position.y);
-	}
+	let orientation = 'N';
 </script>
 
 <h1>Mars kata</h1>
-<!-- <p>Visit me <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> -->
-<table>
-	{#each columns as i}
-		<tr id="r{i}">
-			{#each columns as j}
-				<Cell id={getIdx(i, j)}>{isRoverPosition(i, j) ? 'x' : ''}</Cell>
-			{/each}
-		</tr>
-	{/each}
-</table>
+<Board {position} {orientation} />
